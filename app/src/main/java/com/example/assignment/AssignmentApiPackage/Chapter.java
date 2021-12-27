e035fcaa23b161ccc5e9b2a329df7710628419b2
YashwanthSSBrigosha <yashwanth.s@brigosha.com>
2021-12-27 15:24:10 +0530

    api files added

---
+++ app/src/main/java/com/example/assignment/AssignmentApiPackage/Chapter.java
@@ -0,0 +1,13 @@
+package com.example.assignment.AssignmentApiPackage;
+
+public class Chapter {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/assignment/AssignmentApiPackage/Teacher.java
@@ -0,0 +1,13 @@
+package com.example.assignment.AssignmentApiPackage;
+
+public class Teacher {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
